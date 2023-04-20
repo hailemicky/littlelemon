459a06b9c0a834e047975643f7c37b6fc53861bc
Hailemichael Seleshi <seleshi17@gmail.com>
2023-04-20 14:03:20 +0300

    mysql connection and Model

--- workspace/littlelemon/restaurant/models.py
+++ workspace/littlelemon/restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+
+   
+    title=models.CharField(max_length=255, db_index=True)
+    price=models.DecimalField(max_digits=10, decimal_places=2, db_index=True)
+    inventory=models.SmallIntegerField(db_index=True)
+
+
+class Booking(models.Model):
+    name=models.CharField(max_length=255,db_index=True)
+    no_of_guests=models.SmallIntegerField(db_index=True)
+    bookingdate=models.DateTimeField()
